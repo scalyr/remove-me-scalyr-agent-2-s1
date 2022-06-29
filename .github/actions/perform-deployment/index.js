6bf0390e83948e54a0404f77b38ffbef8727df62
arthur <arthurk@sentinelone.com>
2022-06-29 14:43:31 +0300

    disable tests

--- .github/actions/perform-deployment/index.js
+++ .github/actions/perform-deployment/index.js
@@ -105,7 +105,7 @@ async function performDeployment() {
 
     const deploymentName = core.getInput("deployment-name");
     const cacheVersionSuffix = core.getInput("cache-version-suffix");
-    const cacheDir = path.resolve(path.join("agent_build_output", "deployment_cache", deploymentName));
+    const cacheDir = path.resolve(path.join("agent_build_output", "deployment_cache"));
 
     if (!fs.existsSync(cacheDir)) {
         fs.mkdirSync(cacheDir,{recursive: true})
