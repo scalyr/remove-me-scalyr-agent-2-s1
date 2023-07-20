61b2311c440f4c0d061cf35c2a6b589a933f735c
arthur <arthurk@sentinelone.com>
2023-07-20 02:14:38 +0200

    test

--- scripts/cicd/scalyr-query.sh
+++ scripts/cicd/scalyr-query.sh
@@ -75,6 +75,8 @@ function query_scalyr {
     echo_with_date "Using query '${SCALYR_TOOL_QUERY}'"
 
     python --version
+    /usr/bin/env python --version
+    which python
     RESULT=$(eval "scalyr query '${SCALYR_TOOL_QUERY}' --columns='timestamp,severity,message' --start='20m' --count='100' --output multiline")
     RESULT_LINES=$(echo -e "${RESULT}" | sed '/^$/d' | wc -l)
 
